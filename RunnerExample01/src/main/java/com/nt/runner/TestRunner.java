f311e1d3e3dc55239ec4debfa40ff0d2dcf6976c
amreshpd <amreshpd98@gmail.com>
2024-07-21 17:43:08 +0530

    ak

---
+++ RunnerExample01/src/main/java/com/nt/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package com.nt.runner;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Runner to test..."+Arrays.toString(args));
+	}
+}
